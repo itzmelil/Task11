e22282aa7bf6a621b5cf182f00e3a20859e1a542
Little'll <zlylemarch31@gmail.com>
2026-02-25 14:37:03 +0800

    Add files via upload

--- task11.php
+++ task11.php
@@ -1,62 +1,38 @@
 <?php
 
 class Book {
+
     public $title;
     public $author;
     public $year;
-    public $isAvailable;
+    public $isAvailable = true; 
 
-    function __construct($title, $author, $year){
+    public function __construct($title, $author, $year) {
         $this->title = $title;
-        $this->author = $author; 
+        $this->author = $author;
         $this->year = $year;
+    }
+    public function borrowBook() {  
+        $this->isAvailable = false;
+        echo $this->title . " has been borrowed.<br>";
+    }
+    public function returnBook() {
         $this->isAvailable = true;
+        echo $this->title . " has been returned.<br>";
     }
-
-    public function borrow(){
-        if($this->isAvailable){
-            $this->isAvailable = false;
-            return "You have successfully borrowed {$this->title}.";
-        } else {
-            return "Sorry, {$this->title} is not available.";
-        }
-    }
-
-    public function checkAvailability(){
-        return $this->isAvailable ? "Available" : "Not Available";
-    }
-
-    public function getInfo(){
-        return "
-            <p><strong>Title:</strong> {$this->title}</p>
-            <p><strong>Author:</strong> {$this->author}</p>
-            <p><strong>Year:</strong> {$this->year}</p>
-        ";
+    public function displayBookInfo() {
+        echo "Title: " . $this->title . "<br>";
+        echo "Author: " . $this->author . "<br>";
+        echo "Year: " . $this->year . "<br>";
+        echo "Available: " . ($this->isAvailable ? "Yes" : "No") . "<br><br>";
     }
 }
 
-$book1 = new Book("Jack the Giant Slayer", "Cassandra Luv", 1925);
-$book2 = new Book("366 days", "Jeraldine Wyne", 1960);
-$book3 = new Book("Maria Lyle", "Jack Rizal", 1949);
-
-echo "<div style='font-family: \"Times New Roman\"; font-size: 14px;'>";
-
-
-echo $book1->getInfo();
-echo "<p><strong>Status:</strong> " . $book1->checkAvailability() . "</p>";
-echo "<p>" . $book1->borrow() . "</p>";
-echo "<br/>";
-
-
-echo $book2->getInfo();
-echo "<p><strong>Status:</strong> " . $book2->checkAvailability() . "</p>";
-echo "<p>" . $book2->borrow() . "</p>";
-echo "<br/>";
-
-
-echo $book3->getInfo();
-echo "<p><strong>Status:</strong> " . $book3->checkAvailability() . "</p>";
-echo "<p>" . $book3->borrow() . "</p>";
-echo "<br/>";
+$book1 = new Book("Jack the Giant Slayer", "Jack Rizal", 1995);
+$book1->displayBookInfo();
+$book1->borrowBook();
+$book1->displayBookInfo();
+$book1->returnBook();
+$book1->displayBookInfo();
 
 ?>
